5217264ef7b744f9e678f3908f6b53b77027f8ce
priyanshu <priyanshu10503@gmail.com>
2024-04-30 00:50:59 +0530

    Signinwithpopup to signinwithredirect

--- src/components/Oauth.js
+++ src/components/Oauth.js
@@ -1,5 +1,5 @@
 import { useLocation,useNavigate } from "react-router-dom";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
 import {doc,setDoc,getDoc,serverTimestamp} from 'firebase/firestore'
 import {db} from '../firebase.config'
 import { toast } from 'react-toastify';
@@ -11,7 +11,7 @@ function Oauth() {
         try {
             const auth=getAuth();
             const provider=new GoogleAuthProvider();
-            const result =await signInWithPopup(auth,provider);
+            const result =await signInWithRedirect(auth,provider);
             const user=result.user;
 
             // check for user 
